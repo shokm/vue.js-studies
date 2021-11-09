97641656a0f25bc95968d4a90cb0161b2c72f06c
Shotaro K <26667526+shokm@users.noreply.github.com>
2021-11-10 01:23:40 +0900

    [fix] 見た目を整えた

--- script.js
+++ script.js
@@ -9,48 +9,47 @@ recognition.continuous = true;
 recognition.interimResults = true;
 
 var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'hello!'
-    }
+  el: '#app',
+  data: {
+    message: 'スタートボタンを押してください。'
+  }
 })
 
 var app2 = new Vue({
-    el: '#app2',
-    data: {
-      todos: [
-        { text: 'ここに結果が表示されます' }
-      ]
-    }
-  })
+  el: '#app2',
+  data: {
+    histories: []
+  }
+})
 
-function button_c() {
+function startButton() {
     recognition.start();
     app.message = '認識中……'
 }
 
+/* 参考: https://kesin.hatenablog.com/entry/2013/08/27/Web%E3%82%A2%E3%83%97%E3%83%AA%E3%81%AB%E9%AB%98%E6%A9%9F%E8%83%BD%E3%81%AA%E9%9F%B3%E5%A3%B0%E8%AA%8D%E8%AD%98%E3%82%92%E8%BF%BD%E5%8A%A0%E3%81%99%E3%82%8BWeb_Speech_API */
 recognition.onresult = function(event) {
-    let results = event.results;
-    for (let i = event.resultIndex; i<results.length; i++){
-        //認識の最終結果
-        if(results[i].isFinal){
-            app.message = '認識中……';
-            let res = results[i][0].transcript;
-            app2.todos.push({ text: res })
-        }
-        //認識の中間結果
-        else{
-            app.message = results[i][0].transcript;
-        }
-        
-    }
+  let results = event.results;
+  for (let i = event.resultIndex; i<results.length; i++) {
+      // 認識の最終結果
+      if (results[i].isFinal) {
+          app.message = '認識中……';
+          let res = results[i][0].transcript;
+          app2.histories.push({ text: res })
+      }
+      // 認識の中間結果
+      else{
+          app.message = results[i][0].transcript;
+      }
+      
+  }
 }
 
 recognition.onaudioend = function() {
-    app.message = '音声認識は終了しました。';
-    recognition.start();
+  app.message = '音声認識は終了しました。';
+  recognition.start();
 }
 
 recognition.onsoundend = function() {
-    console.log('音の検出は終わりました。');
-  }
+  console.log('音の検出は終わりました。');
+}
